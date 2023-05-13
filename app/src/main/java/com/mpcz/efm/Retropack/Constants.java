d6531dd48f3df09aa5dda5f9b6c72b9c2f21462d
80nikhil <nikhilkarole808@gmail.com>
2023-05-13 10:12:43 +0530

    new changes

---
+++ app/src/main/java/com/mpcz/efm/Retropack/Constants.java
@@ -0,0 +1,5 @@
+package com.mpcz.efm.Retropack;
+
+public class Constants {
+    public static final String LOGGED_IN = "logged_in";
+}
--- app/src/main/java/com/mpcz/efm/SplashActivity.java
+++ app/src/main/java/com/mpcz/efm/SplashActivity.java
@@ -2,7 +2,12 @@ package com.mpcz.efm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.mpcz.efm.Retropack.Constants;
+import com.mpcz.efm.Util.SharedPref;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(SharedPref.getBool(SplashActivity.this, Constants.LOGGED_IN)==true) {
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }else{
+                    Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        }, 2000);
     }
 }
